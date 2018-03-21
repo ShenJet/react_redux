4255151657bf104a5509e521d6028e54e68fa176
ShenJie <985184097@qq.com>
2018-03-21 17:04:14 +0800

    cmt

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import ClickCounter from './clickCounter';
+import Test0 from './test0';
 import registerServiceWorker from './registerServiceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(
     <ClickCounter />,
     document.getElementById('root')
+)
+
+ReactDOM.render(
+    <Test0 />,
+    document.getElementById('root1')
 )
 registerServiceWorker();
